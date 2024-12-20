8f97bbadec28af6d4f74cd4fb5bbe924c5215a98
MrSimple <muhammadr0jabxujaev@gmail.com>
2024-12-20 13:54:56 +0500

    ok

--- server/api/verification/check.post.ts
+++ server/api/verification/check.post.ts
@@ -1,32 +1,32 @@
-import {prisma} from "~/server/db";
-import twilio from "twilio";
-
-const config = useRuntimeConfig();
-
-const twilioAccountSid = config.public.twilioAccountSid;
-const twilioAuthToken = config.public.twilioAuthToken;
-
-const client = twilio(twilioAuthToken, "AuthToken");
-
-export default defineEventHandler(async (event) => {
-    try {
-        const body = await readBody(event);
-        const {phoneNumber, code} = body;
-
-        if (!phoneNumber || !code) {
-            throw new Error("Missing phone number or code");
-        }
-
-        const verificationCheck = await client.verify.services(twilioAccountSid).verificationChecks.create({
-            to: phoneNumber,
-            code
-        });
-
-        return verificationCheck;
-    } catch (error) {
-        console.error("Error checking verification code:", error);
-        throw error;
-    } finally {
-        await prisma.$disconnect();
-    }
-});
+// import {prisma} from "~/server/db";
+// import twilio from "twilio";
+//
+// const config = useRuntimeConfig();
+//
+// const twilioAccountSid = config.public.twilioAccountSid;
+// const twilioAuthToken = config.public.twilioAuthToken;
+//
+// const client = twilio(twilioAuthToken, "AuthToken");
+//
+// export default defineEventHandler(async (event) => {
+//     try {
+//         const body = await readBody(event);
+//         const {phoneNumber, code} = body;
+//
+//         if (!phoneNumber || !code) {
+//             throw new Error("Missing phone number or code");
+//         }
+//
+//         const verificationCheck = await client.verify.services(twilioAccountSid).verificationChecks.create({
+//             to: phoneNumber,
+//             code
+//         });
+//
+//         return verificationCheck;
+//     } catch (error) {
+//         console.error("Error checking verification code:", error);
+//         throw error;
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// });
--- server/api/verification/send.post.ts
+++ server/api/verification/send.post.ts
@@ -1,34 +1,34 @@
-import twilio from "twilio";
-import {prisma} from "~/server/db";
-
-const config = useRuntimeConfig();
-
-const twilioAccountSid = config.public.twilioAccountSid;
-const twilioAuthToken = config.public.twilioAuthToken;
-
-const client = twilio(twilioAuthToken, "AuthToken");
-
-export default defineEventHandler(async (event) => {
-    try {
-        const body = await readBody(event);
-        const {phoneNumber} = body;
-
-        console.log(twilioAccountSid + " " + twilioAuthToken);
-
-        if (!phoneNumber) {
-            throw new Error("Missing phone number");
-        }
-
-        const verification = await client.verify.services(twilioAccountSid).verifications.create({
-            to: phoneNumber,
-            channel: "sms"
-        });
-
-        return verification;
-    } catch (error) {
-        console.error("Error sending verification code:", error);
-        throw error;
-    } finally {
-        await prisma.$disconnect();
-    }
-});
+// import twilio from "twilio";
+// import {prisma} from "~/server/db";
+//
+// const config = useRuntimeConfig();
+//
+// const twilioAccountSid = config.public.twilioAccountSid;
+// const twilioAuthToken = config.public.twilioAuthToken;
+//
+// const client = twilio(twilioAuthToken, "AuthToken");
+//
+// export default defineEventHandler(async (event) => {
+//     try {
+//         const body = await readBody(event);
+//         const {phoneNumber} = body;
+//
+//         console.log(twilioAccountSid + " " + twilioAuthToken);
+//
+//         if (!phoneNumber) {
+//             throw new Error("Missing phone number");
+//         }
+//
+//         const verification = await client.verify.services(twilioAccountSid).verifications.create({
+//             to: phoneNumber,
+//             channel: "sms"
+//         });
+//
+//         return verification;
+//     } catch (error) {
+//         console.error("Error sending verification code:", error);
+//         throw error;
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// });
